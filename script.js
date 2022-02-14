e57b003f339957750b8115be0c7b47a7890e1a1b
Wend Carlos <wendcarlos18@hotmail.com>
2022-02-14 14:53:53 -0300

    Fiz o estilo da pagina e fiz uma pequena validação no script

--- script.js
+++ script.js
@@ -1,23 +1,53 @@
-const BuscarCep = () => {
-    const cep = document.getElementById('cep').value
-    const url = `http://viacep.com.br/ws/${cep}/json/`
 
-    const endereco = document.getElementById('logradouro')
-    const complemento = document.getElementById('complemento')
-    const bairro = document.getElementById('bairro')
-    const localidade = document.getElementById('localidade')
-    const uf = document.getElementById('uf')
+
+const BuscarCep = () => {
     
-    fetch(url, {method: 'GET'})
-        .then(reponse => {
-            reponse.json()
-            .then(data => {
-                console.log(data)
-                endereco.value = data.logradouro
-                complemento.value = data.complemento
-                bairro.value = data.bairro
-                localidade.value = data.localidade
-                uf.value = data.uf
+    const cep = document.getElementById('cep').value.replace("-", "")
+    if(cep === ""){
+        alert('Digite o cep')
+    }else{
+        const url = `http://viacep.com.br/ws/${cep}/json/`
+
+        const endereco = document.getElementById('logradouro')
+        const complemento = document.getElementById('complemento')
+        const bairro = document.getElementById('bairro')
+        const localidade = document.getElementById('localidade')
+        const uf = document.getElementById('uf')
+
+        
+
+        fetch(url, { method: 'GET' })
+            .then(reponse => {
+                console.log(reponse)
+                reponse.json()
+                    .then(data => {
+                        console.log(data)
+                        endereco.value = data.logradouro
+                        complemento.value = data.complemento
+                        bairro.value = data.bairro
+                        localidade.value = data.localidade
+                        uf.value = data.uf
+                    })
+                    
             })
-        })
+        
+    }
 }
+/* document.getElementById('cep')
+
+if(cep.lenght === ""){
+    limpa_formulário_cep()
+}
+
+function limpa_formulário_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('logradouro').value = ("");
+    document.getElementById('complemento').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('localidade').value = ("");
+    document.getElementById('uf').value = ("");
+} */
+
+
+
+
